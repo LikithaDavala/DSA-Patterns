9fe11e46049c57c34a4d582b50a44ecbe86bed3f
LikithaDavala <likithadavala59@gmail.com>
2026-08-10 22:17:50 +0530

    Forwading Method using Two Pointers O(n²)

--- ForwardLoop/ForwardLoop.js
+++ ForwardLoop/ForwardLoop.js
@@ -1,4 +1,4 @@
-let array =[]
+let array = [];
 let sums = [];
 let count = 0;
 function onClickPushNumbers() {
@@ -8,11 +8,10 @@ function onClickPushNumbers() {
     document.getElementById("divPush").innerHTML = array;
     document.getElementById("txtNumbers").value = null;
 }
-
+// Adding the Two Numbers:
 function onClickArraySumAdd(){
     debugger;
      for(i=0; i<array.length; i++){
-       
         for(j=i+1; j<array.length; j++){
             sums.push(array[i] + array[j]);
             if((array[i] + array[j]) == 7){
@@ -21,14 +20,12 @@ function onClickArraySumAdd(){
         }
     }
     document.getElementById("divArraySumAdd").innerHTML =sums;
-
     alert(count);
 }
-
+// Subtracting the Two Numbers:
 function onClickArraySumSub(){
     debugger;
      for(i=0; i<array.length; i++){
-       
         for(j=i+1; j<array.length; j++){
             sums.push(array[i] - array[j]);
             if(Math.abs(array[i] - array[j]) == 7){
