569e0c16eafc7500277bdcbd938e2267c25ce4ba
LikithaDavala <likithadavala59@gmail.com>
2026-09-19 18:27:18 +0530

    Sliding Window Starts With Vowels

---
+++ SlidingWindowStartsWithVowels/StartsWith.js
@@ -0,0 +1,14 @@
+const string = "acdaeiohkl"
+const vowels = ["a", "e", "i", "o", "u"];
+let k = 3;
+function onClickStartsWith() {
+    debugger;
+    let count = 0;
+    for (i = 0; i <= string.length - k; i++) {
+       let result= string.slice(i, i + k);
+        if(vowels.includes(result[0])){
+            count++;
+        }
+    }
+        console.log(`The number of string starts with a vowels count has ${count} counts`);
+}
