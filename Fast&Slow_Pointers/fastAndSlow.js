7239d50e470beda1714d321268aa1549d7c206c5
LikithaDavala <likithadavala59@gmail.com>
2026-09-21 20:15:28 +0530

    Fast and Slow Pointers

--- Fast&Slow_Pointers/fastAndSlow.js
+++ Fast&Slow_Pointers/fastAndSlow.js
@@ -1,7 +1,7 @@
 const bigArray =[];
 function onClickFastAndSlow(){
-    for(i=0; i<=500000 ; i++){
-        bigArray.push(Math.trunc(Math.random()*Math.pow(10,3)));
+    for(i=0; i<5 ; i++){
+        bigArray.push(Math.trunc(Math.random()*Math.pow(10,6)));
     }
     console.log(bigArray)
 }
