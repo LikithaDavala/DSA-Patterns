cc8d7bcc1050915f75ffcbbfd40d37a83df004dd
LikithaDavala <likithadavala59@gmail.com>
2026-09-25 20:58:00 +0530

    Starts With

--- SlidingWindowStartsWithVowels/StartsWith.js
+++ SlidingWindowStartsWithVowels/StartsWith.js
@@ -7,9 +7,8 @@ function onClickStartsWith() {
     for (i = 0; i <= string.length - k; i++) {
        let result= string.slice(i, i + k);
         if(vowels.includes(result[0])){
-           let result = count++;
+        count++;
         }
     }
         console.log(`The number of string starts with a vowels count has ${count} counts`);
-        document.getElementById("divStartsWith").innerHTML = result;
 }
